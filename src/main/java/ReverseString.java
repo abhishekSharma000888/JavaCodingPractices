0ab4f189b7ae85529844ce9710be4c3c95ad98b0
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-08-13 18:09:08 +0530

    added practices

---
+++ src/main/java/ReverseString.java
@@ -0,0 +1,25 @@
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        String input = "India is great";
+        // output = Great is India
+
+
+        String[] words = input.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for(int i= words.length-1; i>=0; i--){
+
+            result.append(words[i]);
+            result.append(" ");
+        }
+
+        String output = result.toString().trim();
+        output = output.substring(0,1).toUpperCase()+ output.substring(1);
+
+
+        System.out.println(output);
+
+    }
+}
---
+++ src/main/java/SortArrays.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SortArrays {
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {7, -5, 3, 8, -4, 11, -19, 21};
+        int[] arr2 = {6, 13, -7, 0, -4, 3, -5};
+
+        List<Integer> l1 = Stream.concat(Arrays.stream(arr1)
+                        .boxed(), Arrays.stream(arr2).boxed())
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        System.out.print(l1);
+
+    }
+    }
