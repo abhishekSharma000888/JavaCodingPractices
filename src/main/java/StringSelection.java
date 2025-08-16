c728e134f2a4b72ece4b862da928ee46ab156403
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-08-16 16:13:17 +0530

    added practice

---
+++ src/main/java/StringSelection.java
@@ -0,0 +1,28 @@
+public class StringSelection {
+
+    // Input = aabbcccaabbb
+    //  Output = abcab
+    // abc
+    public static void main(String[] args) {
+        // Input = aabbcccaabbb
+        //  Output = abcab
+        // abc
+
+        String input = "d";
+
+        StringBuilder output = new StringBuilder();
+
+        output.append(input.charAt(0));
+
+        for (int i = 0; i < input.length(); i++) {
+
+            char current = input.charAt(i);
+
+            if (output.indexOf(String.valueOf(current)) == -1) {
+                output.append(current);
+            }
+        }
+
+        System.out.println("The output is: " + output);
+    }
+}
